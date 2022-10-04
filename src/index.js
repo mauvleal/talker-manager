cd69d7b354c0486a513120704e7c15db8e4ab31d
Mauricio Leal <mauricio_leal2010@yahoo.com.br>
2022-10-04 17:35:59 -0300

    requisito 4

--- src/index.js
+++ src/index.js
@@ -28,13 +28,20 @@ app.get('/talker/:id', async (request, response) => {
 
 app.post('/login', (request, response) => {
   const { email, password } = request.body;
-
+  const re = /\S+@\S+\.\S+/;
   if (!email) {
     return response.status(400).json({ message: 'O campo "email" é obrigatório' });
   }
   if (!password) {
     return response.status(400).json({ message: 'O campo "password" é obrigatório' });
   } 
+  if (!re.test(email)) {
+    return response.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  if (password.length < 6) {
+return response.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+
   return response.status(200).json({ token: generateToken() });
 });
 
